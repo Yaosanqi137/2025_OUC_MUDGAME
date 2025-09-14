e655e7a7b1c7676e6f5ffd0f64271b8992ef80d6
Yaosanqi137 <zxc18023571263@outlook.com>
2025-09-15 02:08:34 +0800

    joke: i forgot something...

--- src/game/main/GameStory/Story/chapter1.h
+++ src/game/main/GameStory/Story/chapter1.h
@@ -44,7 +44,7 @@ namespace Chapter1 {
             [](Game& game) {
                 game.getDialog().clearHistory();
                 game.getPlayer().setLocation("拳击馆外");
-                game.setGameState(GameState::InGame);
+                game.setGameState(GameState::InStory);
             }
         }
     });
